af495b7f3f4e369092bf4b8960d49a3a0994c96d
felix_liu <2304674553@qq.com>
2020-01-08 13:19:28 +0800

    fix cpm model tensorboard image log error

--- callbacks_model_cpm.py
+++ callbacks_model_cpm.py
@@ -18,7 +18,8 @@ import tensorflow as tf
 
 
 def get_check_pointer_callback(model_path, output_name):
-    checkpoint_path = os.path.join(model_path, output_name + ".hdf5")  # ".ckpt"
+    checkpoint_path = os.path.join(
+        model_path, output_name + ".hdf5")  # ".ckpt"
     check_pointer_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                                 save_weights_only=False,
                                                                 verbose=1)
@@ -33,7 +34,7 @@ def get_tensorboard_callback(log_path, output_name):
     return tensorboard_callback
 
 
-def get_img_tensorboard_callback(log_path, output_name, images, labels, model):
+def get_img_tensorboard_callback(log_path, output_name, inputs, labels, model):
     # tensorboard image
     def _show_image_for_debugging(numpy_img):
         from PIL import Image
@@ -44,25 +45,29 @@ def get_img_tensorboard_callback(log_path, output_name, images, labels, model):
         img.save('my.png')
         img.show()
 
-    file_writer = tf.summary.create_file_writer(os.path.join(log_path, output_name))
+    file_writer = tf.summary.create_file_writer(
+        os.path.join(log_path, output_name))
 
     def log_tensorboard_predicted_images(epoch, logs):
         # Use the model to predict the values from the validation dataset.
         # batch_size = 6
         # images, labels = dataloader_valid.get_images(80, batch_size)
-        predictions = model.predict(images)
+        predictions = model.predict(inputs)
+        predictions = predictions[-1]
 
         # Log the confusion matrix as an image summary.
         from data_loader.pose_image_processor import PoseImageProcessor
 
         # summary_str = []
+        images = inputs['input_1']
         predicted_images = []
         for i in range(images.shape[0]):
             image = images[i, :, :, :]
             label = labels[i, :, :, :]
             prediction = predictions[i, :, :, :]
 
-            numpy_img = PoseImageProcessor.display_image(image, true_heat=label, pred_heat=prediction, as_numpy=True)
+            numpy_img = PoseImageProcessor.display_image(
+                image, true_heat=label, pred_heat=prediction, as_numpy=True)
 
             numpy_img = numpy_img / 255
 
@@ -70,9 +75,11 @@ def get_img_tensorboard_callback(log_path, output_name, images, labels, model):
 
         with file_writer.as_default():
             # Don't forget to reshape.
-            tf.summary.image("predict from validation dataset", predicted_images, max_outputs=10, step=epoch)
+            tf.summary.image("predict from validation dataset",
+                             predicted_images, max_outputs=10, step=epoch)
 
     # Define the per-epoch callback.
-    img_tensorboard_callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=log_tensorboard_predicted_images)
+    img_tensorboard_callback = tf.keras.callbacks.LambdaCallback(
+        on_epoch_end=log_tensorboard_predicted_images)
 
     return img_tensorboard_callback
